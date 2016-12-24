65edfb356a06e29f7dba4ce0588dc85f3b3e3be7
vadim-pos <v.postanovkin@gmail.com>
2016-12-24 11:57:20 +0300

    fix

--- APM/app/products/product-filter.pipe.ts
+++ APM/app/products/product-filter.pipe.ts
@@ -6,9 +6,9 @@ import { IProduct } from './product';
 	name: 'productFilter'
 })
 export class ProductFilterPipe implements PipeTransform {
-	
+
 	transform(value: IProduct[], filterBy: string): IProduct[] {
-		filterBy = filterBy.toLocaleLowerCase() : null;
+		filterBy = filterBy ? filterBy.toLocaleLowerCase() : null;
 		return filterBy ? value.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1) : value;
 	}
 }

2299a1ad2ad5b4834e8f69f2a96e3a2f3c121733
vadim-pos <v.postanovkin@gmail.com>
2017-01-08 17:39:10 +0300

    app completed

--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
-import { ProductListComponent }  from './products/product-list.component';
-import { ProductFilterPipe }  from './products/product-filter.pipe';
-import { StarComponent }  from './shared/star.component';
+import { WelcomeComponent }  from './home/welcome.component';
+import { ProductModule }  from './products/product.module';
 
 @NgModule({
   imports: [
   	BrowserModule,
-  	FormsModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ProductModule
   ],
   declarations: [
   	AppComponent,
-  	ProductListComponent,
-  	ProductFilterPipe,
-    StarComponent
+    WelcomeComponent
   ],
   bootstrap: [ AppComponent ]
 })
